3a8b7279b510a002d836793af0dcc8f2d7c822d5
Emilio Almansi <ealmansi@gmail.com>
2016-05-27 11:36:31 -0300

    Added related pages call to mock server.

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,24 +1,58 @@
+// Get dependencies.
+var _ = require("underscore");
+var bodyParser = require("body-parser");
 var express = require("express");
 var path = require("path");
-var bodyParser = require("body-parser");
+var pgp = require('pg-promise')();
+var pgpPS = require('pg-promise').PreparedStatement;
 
+// Define database object.
+var db = pgp({
+  host: 'pg-19e90881.prj-l68cwb.aivencloud.com',
+  port: 24143,
+  database: 'd6h3a0r9',
+  user: 'u9i6g3r1',
+  password: 'i9po3nsdzszjjtgl',
+  ssl: true
+});
+
+// Build prepared statements.
+var getRelatedPagesQuery = "";
+getRelatedPagesQuery += "SELECT ";
+getRelatedPagesQuery += "  po1.page_id AS page_id, ";
+getRelatedPagesQuery += "  COUNT(po4.page_id) * 1.0 / COUNT(*) as score ";
+getRelatedPagesQuery += "FROM enwiki20160501.page_outlinks po1 ";
+getRelatedPagesQuery += "LEFT join enwiki20160501.page_outlinks po4 ";
+getRelatedPagesQuery += "ON po4.page_id = $1 and po1.outlink = po4.outlink ";
+getRelatedPagesQuery += "WHERE po1.page_id IN ( ";
+getRelatedPagesQuery += "  SELECT outlink ";
+getRelatedPagesQuery += "  FROM enwiki20160501.page_outlinks po2 ";
+getRelatedPagesQuery += "  WHERE page_id = $1 ";
+getRelatedPagesQuery += ") AND po1.page_id IN ( ";
+getRelatedPagesQuery += "  SELECT page_id ";
+getRelatedPagesQuery += "  FROM enwiki20160501.page_outlinks po3 ";
+getRelatedPagesQuery += "  WHERE outlink = $1 ";
+getRelatedPagesQuery += ") AND po1.page_id <> $1 ";
+getRelatedPagesQuery += "GROUP BY po1.page_id ";
+getRelatedPagesQuery += "ORDER BY score desc ";
+getRelatedPagesQuery += "LIMIT 10 ";
+var getRelatedPagesStatement = new pgpPS('get-related-pages', getRelatedPagesQuery);
+
+// Initialize server.
 var app = express();
 app.use(bodyParser.json());
-
-// Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function () {
   console.log("App now running on port", port);
 });
 
 // Define API.
-app.get("/pages", function(req, res) {
-  var jsonResult = [{'id': 0}, {'id': 1}, {'id': 2}, {'id': 3}];
-  res.status(200).json(jsonResult);
+app.get("/related/:page", function(req, res) {
+  var pageId = req.params.page;
+  getRelatedPagesStatement.values = pageId;
+  db.any(getRelatedPagesStatement)
+    .then(function (data) {
+        var pageIds = _.pluck(data, 'page_id');
+        res.status(200).json(pageIds);
+    })
 });
-
-//
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
